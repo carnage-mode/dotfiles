eb3fd7b1a029d04179ec633e95e5c41eeb50f187
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-06-22 14:06:51 +0300

    Use local leader for barbar operations

--- dot_config/nvim/lua/plugins/barbar.lua
+++ dot_config/nvim/lua/plugins/barbar.lua
@@ -8,19 +8,19 @@ return {
     local opts = { noremap = true, silent = true }
 
     -- Move to previous/next
-    map("n", "<A-h>", "<Cmd>BufferPrevious<CR>", { desc = "Previous Buffer", table.unpack(opts) })
-    map("n", "<A-l>", "<Cmd>BufferNext<CR>", { desc = "Next Buffer", table.unpack(opts) })
+    map("n", "<LocalLeader>h", "<Cmd>BufferPrevious<CR>", { desc = "Previous Buffer", table.unpack(opts) })
+    map("n", "<LocalLeader>l", "<Cmd>BufferNext<CR>", { desc = "Next Buffer", table.unpack(opts) })
     -- Re-order to previous/next
-    map("n", "<A-H>", "<Cmd>BufferMovePrevious<CR>", { desc = "Move Buffer Right", table.unpack(opts) })
-    map("n", "<A-L>", "<Cmd>BufferMoveNext<CR>", { desc = "Move Buffer Left", table.unpack(opts) })
+    map("n", "<LocalLeader>H", "<Cmd>BufferMovePrevious<CR>", { desc = "Move Buffer Right", table.unpack(opts) })
+    map("n", "<LocalLeader>L", "<Cmd>BufferMoveNext<CR>", { desc = "Move Buffer Left", table.unpack(opts) })
     -- Pin/unpin buffer
-    map("n", "<A-p>", "<Cmd>BufferPin<CR>", { desc = "Pin Buffer", table.unpack(opts) })
+    map("n", "<LocalLeader>p", "<Cmd>BufferPin<CR>", { desc = "Pin Buffer", table.unpack(opts) })
     -- Close buffer
-    map("n", "<C-c>", "<Cmd>BufferClose<CR>", { desc = "Close Buffer", table.unpack(opts) })
+    map("n", "<LocalLeader>c", "<Cmd>BufferClose<CR>", { desc = "Close Buffer", table.unpack(opts) })
     -- Restore buffer
-    map("n", "<A-S-c>", "<Cmd>BufferRestore<CR>", { desc = "Restore Buffer", table.unpack(opts) })
+    map("n", "<LocalLeader>r", "<Cmd>BufferRestore<CR>", { desc = "Restore Buffer", table.unpack(opts) })
     -- Magic buffer-picking mode
-    map("n", "<C-p>", "<Cmd>BufferPick<CR>", { desc = "Magic Buffer Select Mode", table.unpack(opts) })
+    map("n", "<LocalLeader>m", "<Cmd>BufferPick<CR>", { desc = "Magic Buffer Select Mode", table.unpack(opts) })
 
     require("barbar").setup()
   end,
