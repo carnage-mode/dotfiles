3c00d06710e5f1bf782cb387a5ca46d197811522
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-06-24 17:07:49 +0300

    Add gitsigns mappings

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -1,4 +1,9 @@
 return {
   "lewis6991/gitsigns.nvim",
-  opts = {},
+
+  config = function()
+    require("gitsigns").setup()
+    vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+    vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+  end,
 }
