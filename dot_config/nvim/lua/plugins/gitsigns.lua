3fe6d42bf5ac55819851bbb0682d1cae822a3f15
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-06-26 14:31:23 +0300

    Change git line blame keybind

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -4,6 +4,6 @@ return {
   config = function()
     require("gitsigns").setup()
     vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Preview Hunk" })
-    vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle current line blame" })
+    vim.keymap.set("n", "<leader>tg", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Current Git Line Blame" })
   end,
 }
