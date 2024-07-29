aedb62b7c141ed3507d1bc58af02e06f8f5e2308
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-07-30 01:50:36 +0300

    Show oil preview by default

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -14,6 +14,16 @@ return {
       },
     }
     vim.keymap.set("n", "<leader>bf", "<CMD>Oil --float<CR>", { desc = "[B]rowse [F]iles" })
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "OilEnter",
+      callback = vim.schedule_wrap(function(args)
+        local oil = require "oil"
+        if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
+          oil.open_preview()
+        end
+      end),
+    })
   end,
 
   dependencies = { "nvim-tree/nvim-web-devicons" },
