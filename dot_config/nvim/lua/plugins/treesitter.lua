ac82ab2b7adc75d3fec6da9e72a0e52c52dd1752
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-07-30 01:11:52 +0300

    Add treesitter support for laravel blade files

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,14 @@ return { -- Treesitter config
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
 
+  init = function()
+    vim.filetype.add {
+      pattern = {
+        [".*%.blade%.php"] = "blade",
+      },
+    }
+  end,
+
   config = function()
     local configs = require "nvim-treesitter.configs"
 
@@ -35,6 +43,18 @@ return { -- Treesitter config
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
+    }
+
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+    parser_config.blade = {
+      install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
+      },
+
+      filetype = "blade",
     }
   end,
 }
