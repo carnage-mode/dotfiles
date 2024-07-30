ee5acfa99f6c2b852b8994e39da3d365db07106a
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-07-31 02:22:46 +0300

    Change mappings for mini.surround

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -19,7 +19,20 @@ return {
       },
     }
 
-    require("mini.surround").setup()
+    require("mini.surround").setup {
+      mappings = {
+        add = "ma", -- Add surrounding in Normal and Visual modes
+        delete = "md", -- Delete surrounding
+        find = "mf", -- Find surrounding (to the right)
+        find_left = "mF", -- Find surrounding (to the left)
+        highlight = "mh", -- Highlight surrounding
+        replace = "mr", -- Replace surrounding
+        update_n_lines = "mn", -- Update `n_lines`
+
+        suffix_last = "", -- Suffix to search with "prev" method
+        suffix_next = "", -- Suffix to search with "next" method
+      },
+    }
 
     require("mini.move").setup {
       mappings = {
