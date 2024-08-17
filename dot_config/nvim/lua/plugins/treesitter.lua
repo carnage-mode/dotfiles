c92976000626d7b9757f4f077e80dc896367fab7
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-08-17 18:40:53 +0300

    Replace mini.ai with treesitter textobjects

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,10 @@ return { -- Treesitter config
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
 
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+
   init = function()
     vim.filetype.add {
       pattern = {
@@ -43,6 +47,34 @@ return { -- Treesitter config
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
+
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+
+          keymaps = {
+            ["aa"] = { query = "@attribute.outer", desc = "Around attribute" },
+            ["ia"] = { query = "@attribute.inner", desc = "Inside attribute" },
+            ["ac"] = { query = "@comment.outer", desc = "Around comment" },
+            ["ic"] = { query = "@comment.inner", desc = "Inside comment" },
+            ["af"] = { query = "@function.outer", desc = "Around function" },
+            ["if"] = { query = "@function.inner", desc = "Inside function" },
+            ["ap"] = { query = "@parameter.outer", desc = "Around parameter" },
+            ["ip"] = { query = "@parameter.inner", desc = "Inside parameter" },
+            ["as"] = { query = "@class.outer", desc = "Around struct/class" },
+            ["is"] = { query = "@class.inner", desc = "Inside struct/class" },
+          },
+
+          selection_modes = {
+            ["@parameter.outer"] = "v", -- charwise
+            ["@function.outer"] = "V", -- linewise
+            ["@class.outer"] = "<c-v>", -- blockwise
+          },
+
+          include_surrounding_whitespace = true,
+        },
+      },
     }
 
     local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
