014570db7eeee4da25550a08e9232e9fe0fbbe82
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-08-18 04:39:49 +0300

    Resolve keymap conflicts

--- dot_config/nvim/lua/plugins/comment.lua
+++ dot_config/nvim/lua/plugins/comment.lua
@@ -1,4 +1,14 @@
 return {
   "numToStr/Comment.nvim",
-  opts = {},
+
+  config = function()
+    require("Comment").setup()
+    vim.keymap.del("n", "gc")
+    vim.keymap.del("n", "gb")
+    local wk = require "which-key"
+    wk.add {
+      { "gb", group = "Comment toggle blockwise" },
+      { "gc", group = "Comment toggle linewise" },
+    }
+  end,
 }
