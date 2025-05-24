f771c0f17324be8548bb4eb2cf51a42cd696b771
Beamanuel Tesfaye <beta-stage@tutanota.com>
2025-05-24 18:39:21 +0300

    Add winbar using lualine

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -5,8 +5,32 @@ return {
   config = function()
     require("lualine").setup {
       options = {
+        icons_enabled = false,
         section_separators = { left = "", right = "" },
         component_separators = { left = "|", right = "|" },
+      },
+      winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {
+          {
+            "filename",
+            path = 3,
+            newfile_status = true,
+            shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+            color = { fg = "light", bg = "dark", gui = "italic" },
+            padding = 2,
+            symbols = {
+              modified = "[+]",
+              readonly = "[-]",
+              unnamed = "[No Name]",
+              newfile = "[New]",
+            },
+          },
+        },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
       },
     }
   end,
