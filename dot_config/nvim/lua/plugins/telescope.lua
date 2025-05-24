4f11f5361e9f2d37a18350999db159207839ed9c
Beamanuel Tesfaye <beta-stage@tutanota.com>
2025-05-24 18:39:08 +0300

    Add telescope-based project management

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-telescope/telescope-project.nvim",
   },
 
   config = function()
@@ -24,9 +25,17 @@ return {
     }
 
     require("telescope").load_extension "fzf"
+    require("telescope").load_extension "project"
 
     local builtin = require "telescope.builtin"
+
     vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [f]iles in current working dir" })
+    vim.keymap.set(
+      "n",
+      "<leader>fp",
+      ":lua require'telescope'.extensions.project.project{}<CR>",
+      { desc = "[F]ind [p]rojects" }
+    )
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[G]rep strings in current working dir" })
     vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind [buffers]" })
     vim.keymap.set("n", "<leader>q", builtin.diagnostics, { desc = "Open diagnostic [Q]uickfix list" })
